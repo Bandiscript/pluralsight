1d21f9d8d699f8d86cf90fbea39b6aa5dbb761b0
Andras Gyula Csontos <acsontos@outlook.hu>
2023-02-06 01:24:36 +0100

    acme pluralsightos komponens létrehozása, flexboxok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './angular.module';
+import { ButtonPeldak } from './buttons/button-component';
+import { SelectOverviewExample } from './select/select.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { AcmeComponent } from './acme/acme.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonPeldak,
+    SelectOverviewExample,
+    HeroesComponent,
+    AcmeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/buttons/button-component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic buttons
+ */
+@Component({
+  selector: 'button-peldak',
+  templateUrl: 'button-component.html',
+  styleUrls: ['button-component.scss'],
+})
+export class ButtonPeldak {}
